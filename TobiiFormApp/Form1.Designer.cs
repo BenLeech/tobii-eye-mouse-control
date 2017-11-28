229ad05cecc8fe90182129ea3fec984cf7362221
Ben Leech <b.leech@hotmail.ca>
2017-11-27 22:16:01 -0600

    Created git repo for existing project

---
+++ TobiiFormApp/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace TobiiFormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(288, 70);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Gaze Smooth Filter";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(209, 31);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(70, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Unfiltered";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(78, 31);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(114, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Weighted Average";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 31);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(52, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Alpha";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Gaze Point X: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Gaze Point Y: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(107, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(107, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(273, 57);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Toggle gaze mouse control";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 230);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        public System.Windows.Forms.RadioButton radioButton3;
+        public System.Windows.Forms.RadioButton radioButton2;
+        public System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.Label label3;
+        public System.Windows.Forms.Label label4;
+        public System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TobiiFormApp/Program.cs
@@ -0,0 +1,148 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tobii.Interaction;
+using WindowsInput;
+
+namespace TobiiFormApp
+{
+    static class Program
+    {
+        private static Point prevPos;
+        private static bool hasPrevPos;
+        private static float alpha = 0.3f;
+
+        private enum filters {Smooth, Averaged, Unfiltered};
+
+        private static bool enableGazeMouseControl = false;
+        private static int currentFilter;
+
+        private static Form1 form;
+        private static IKeyboardMouseEvents m_GlobalHook;
+
+        private static void GlobalHookKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                InputSimulator inputSimulator = new InputSimulator(); //!!!!!
+                inputSimulator.Mouse.LeftButtonDown();
+            }
+        }
+
+        private static void GlobalHookKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                InputSimulator inputSimulator = new InputSimulator(); //!!!!!
+                inputSimulator.Mouse.LeftButtonUp();
+            }
+        }
+
+        //Moves the mouse cursor and applies filter based on the currently selected setting
+        private static void moveCursor(int x, int y)
+        {
+            Cursor.Position = SmoothFilter(new Point(x,y));
+        }
+
+        private static void subscribeGlobalKeyHook()
+        {
+            m_GlobalHook = Hook.GlobalEvents();
+            m_GlobalHook.KeyDown += GlobalHookKeyDown;
+            m_GlobalHook.KeyUp += GlobalHookKeyUp;
+        }
+
+        private static void unsubscribeGlobalKeyHook()
+        {
+            m_GlobalHook.KeyDown -= GlobalHookKeyDown;
+            m_GlobalHook.KeyUp -= GlobalHookKeyUp;
+            m_GlobalHook.Dispose();
+        }
+
+        //Applies a filter to the point based on currently selected setting
+        private static Point SmoothFilter(Point point)
+        {
+            //checks which filter is selected
+            checkFilterSettings();
+
+            Point filteredPoint = point;
+
+            if (!hasPrevPos)
+            {
+                prevPos = point;
+                hasPrevPos = true;
+            }
+
+            if(currentFilter == (int)filters.Smooth)
+            {
+                filteredPoint = new Point((int)((point.X * alpha) + (prevPos.X * (1.0f - alpha))),
+                                                (int)((point.Y * alpha) + (prevPos.Y * (1.0f - alpha))));
+            }else if(currentFilter == (int)filters.Averaged)  //takes the average of the current point and the previous point
+            {
+                filteredPoint = new Point((point.X + prevPos.X) / 2, (point.Y + prevPos.Y) / 2);
+            }
+
+            prevPos = filteredPoint; //set the previous point to current point
+
+            return filteredPoint;
+        }
+
+        private static void toggleGazeMouse(object sender, EventArgs e)
+        {
+            enableGazeMouseControl = !enableGazeMouseControl;
+        }
+
+        private static void checkFilterSettings()
+        {
+            if (form.radioButton1.Checked) //Smooth alpha filter
+            {
+                currentFilter = (int)filters.Smooth;
+            }
+            else if (form.radioButton2.Checked) //Averaged filter
+            {
+                currentFilter = (int)filters.Averaged;
+            }
+            else //unfiltered
+            {
+                currentFilter = (int)filters.Unfiltered;
+            }
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            var host = new Host();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            form = new Form1();
+
+            subscribeGlobalKeyHook();
+
+            //handle the 'toggle gaze control' button event
+            form.button1.Click += new System.EventHandler(toggleGazeMouse);
+
+            //create the data stream
+            var gazePointDataStream = host.Streams.CreateGazePointDataStream(Tobii.Interaction.Framework.GazePointDataMode.LightlyFiltered);
+            gazePointDataStream.GazePoint((x, y, _) =>
+            {
+                if (enableGazeMouseControl)
+                {
+                    moveCursor((int)x, (int)y);
+                    //update the form labels with gaze coordinate
+                    form.label3.Invoke((MethodInvoker)(() => form.label3.Text = x.ToString()));
+                    form.label4.Invoke((MethodInvoker)(() => form.label4.Text = y.ToString()));
+                }
+                
+            });
+
+            Application.Run(form);
+        }
+    }
+}
